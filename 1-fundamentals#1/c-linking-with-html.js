41add533e55c9384e6bdb2eb2475fdb3a14ff794
abdelrhman ahmed kamal <abdelrhman.ahmed.nassar@gmail.com>
2022-05-13 13:57:41 +0200

    add the fundemetals section

---
+++ 1-fundamentals#1/c-linking-with-html.js
@@ -0,0 +1,21 @@
+/*
+external 
+
+
+<body>
+
+<script src="path.js"></script>
+</body>
+
+
+we put it in the end so the browser read all the DOM then manipulate it with js 
+because it can't manipulate it without seeing it 
+
+or inline script 
+
+<head>
+  <script> 
+  let a = 10;
+  </script>
+</head>
+*/
